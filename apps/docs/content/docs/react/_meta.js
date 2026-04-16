d793692a11dad94644a3ae11e49144a833e06044
kimcy <kimcy1@gmail.com>
2026-04-16 15:58:27 +0900

    feat: 문서에서 '영속화'를 'Persistence'로 변경 및 UI 관련 문서 제목 수정

--- apps/docs/content/docs/react/_meta.js
+++ apps/docs/content/docs/react/_meta.js
@@ -1,6 +1,6 @@
 export default {
   'use-chat': 'useChat',
-  persistence: '영속화',
+  persistence: 'Persistence',
   'optimistic-ui': '낙관적 UI',
   markdown: 'Markdown 렌더러',
   'sse-debug': 'SSE 디버그 패널',
