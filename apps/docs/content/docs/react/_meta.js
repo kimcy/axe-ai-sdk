4f4b1e2d424e109879b05d4c4d757db75d10d4cd
kimcy <kimcy1@gmail.com>
2026-04-23 09:11:32 +0900

    fix: '낙관적 UI'를 'Optimistic UI'로 수정 및 문서 제목 변경

--- apps/docs/content/docs/react/_meta.js
+++ apps/docs/content/docs/react/_meta.js
@@ -1,7 +1,7 @@
 export default {
   'use-chat': 'useChat',
   persistence: 'Persistence',
-  'optimistic-ui': '낙관적 UI',
+  'optimistic-ui': 'Optimistic UI',
   markdown: 'Markdown 렌더링',
   'sse-debug': 'SSE 디버그 패널',
 }
